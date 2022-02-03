be9b0cb111fd921c4f68d17da103c91434dffe71
finn-01 <nguyensam576@gmail.com>
2022-02-03 12:11:44 +0700

    done

--- src/main/java/com/example/springreactjs/controller/UserController.java
+++ src/main/java/com/example/springreactjs/controller/UserController.java
@@ -3,12 +3,14 @@ package com.example.springreactjs.controller;
 import com.example.springreactjs.model.User;
 import com.example.springreactjs.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("api/")
 public class UserController {
